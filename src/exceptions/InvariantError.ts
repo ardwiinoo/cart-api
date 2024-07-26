6cd82b47dec925670b79befb4d07ea239cf5b080
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-07-26 23:44:23 +0700

    feat: add invariant error

---
+++ src/exceptions/InvariantError.ts
@@ -0,0 +1,13 @@
+import ClientError from "./ClientError";
+
+class InvariantError extends ClientError {
+
+    public name: string
+
+    constructor(message: string) {
+        super(message)
+        this.name = 'InvariantError'
+    }
+}
+
+export default InvariantError
