e5f0573dc6de550a1ec114bedb3ff6f7b1cf3f66
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-07-26 23:44:13 +0700

    feat: add custom client error

---
+++ src/exceptions/ClientError.ts
@@ -0,0 +1,13 @@
+class ClientError extends Error {
+
+    public statusCode: number
+    public name: string
+
+    constructor(message: string, statusCode: number = 400) {
+        super(message)
+        this.statusCode = statusCode
+        this.name = 'ClientError'
+    }
+}
+
+export default ClientError
