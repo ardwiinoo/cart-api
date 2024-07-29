9f14140cde083263d504af50c9719eb794f5f736
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-07-29 19:11:24 +0700

    feat: add update delete token route

--- src/api/authentications/routes.ts
+++ src/api/authentications/routes.ts
@@ -5,6 +5,8 @@ const routes = (handler: AuthenticationsHandler) => {
     const v1 = Router()
 
     v1.post('/authentications', handler.postAuthHandler)
+    v1.put('/authentications', handler.putAuthHandler)
+    v1.delete('/authentications', handler.deleteAuthHandler)
 
     return v1
 }
