169b70c039cf016fd7cfc0a07705d1024ac46b06
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-07-30 19:15:32 +0700

    feat: add producer service

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -3,9 +3,11 @@ import { UsersValidator } from "@/validator"
 import { Express } from "express"
 import UsersHandler from "./handler"
 import routes from "./routes"
+import { ProducerService } from "@/services/rabbitmq/ProducerService"
 
 export interface UsersOptions {
-    service: UsersService
+    usersService: UsersService
+    producerService: typeof ProducerService
     validator: typeof UsersValidator
 }
 
@@ -13,7 +15,7 @@ export const users = {
     name: 'users',
     version: '1.0.0',
     register: (app: Express, options: UsersOptions) => {
-        const usersHandler = new UsersHandler(options.service, options.validator)
+        const usersHandler = new UsersHandler(options.usersService, options.producerService, options.validator)
         app.use('/api', routes(usersHandler))
     }
 }
