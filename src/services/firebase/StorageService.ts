10cb501b15e069b0014b163e9e029cbb7adce7b4
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-07-31 22:24:52 +0700

    feat: add firebase service for fle storage

---
+++ src/services/firebase/StorageService.ts
@@ -0,0 +1,40 @@
+import { env } from '@/utils/env'
+import admin from 'firebase-admin'
+import serviceAccount from '@/services/firebase/creds/merce-api-firebase-adminsdk.json'
+import { nanoid } from 'nanoid'
+
+export class StorageService {
+    private storage: admin.storage.Storage
+    private bucketName: string
+
+    constructor() {
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+            storageBucket: env.get('FIREBASE_STORAGE_PATH').toString()
+        })
+
+        this.storage = admin.storage()
+        this.bucketName = this.storage.bucket().name
+    }
+
+    async uploadFile(fileBuffer: Buffer, destFolder: string, filename: string, contentType: string) {
+        const bucket = this.storage.bucket(this.bucketName)
+        const file = bucket.file(`${destFolder}/${nanoid(16)}-${filename}`)
+
+        await file.save(fileBuffer, {
+            resumable: false,
+            metadata: {
+                contentType: contentType
+            }
+        })
+
+        return file.publicUrl()
+    }
+
+    async deleteFile(filePath: string) {
+        const bucket = this.storage.bucket(this.bucketName)
+        const file = bucket.file(filePath)
+
+        await file.delete()
+    }
+}
