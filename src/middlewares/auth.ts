05942d9192427be3956a719f1eda4d3f1fc4b649
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-08-01 18:19:24 +0700

    feat: add auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+import { AuthenticationError } from "@/exceptions";
+import { UsersService } from "@/services/postgres";
+import { TokenManager as tokenManager } from "@/tokenize/TokenManager";
+import { User } from "@/utils/types";
+import { NextFunction, Request, Response } from "express";
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: User
+        }
+    }
+}
+
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const authHeader = req.headers.authorization
+        if(!authHeader) throw new Error()
+
+        const token = authHeader.replace('Bearer', '')
+        const { id } = tokenManager.verifyAccessToken(token)
+
+        const usersService = new UsersService()
+        const user = await usersService.getUserById(id)
+        req.user = user
+
+        next()
+    } catch (err) {
+        next(new AuthenticationError('Unauthorized'))
+    }
+}
