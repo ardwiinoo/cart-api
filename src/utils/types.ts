96ea484bb2ea9b901f8af0b74dfcfc05f4f25ba2
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-08-01 18:22:18 +0700

    feat: add user interface

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,5 +1,13 @@
-
 export enum Roles {
     ADMIN = 1,
     CUSTOMER = 2
 }
+
+export interface User {
+    id: string
+    email: string
+    avatar_url: string
+    phone: string
+    role_id: number
+    is_verified: boolean
+} 
