03068cfe5ceb4bb9247f1428555b40adb05795e4
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-08-04 14:09:53 +0700

    feat: move types

--- src/validator/products/schema.ts
+++ src/validator/products/schema.ts
@@ -1,10 +1,8 @@
 import { z } from "zod";
 
 export const PostProductPayloadSchema = z.object({
-    name: z.string().min(1),
-    price: z.number().min(1),
-    stock: z.number().min(1),    
-    description: z.string().min(1)
+    name: z.string().min(1, { message: 'Name is required' }),
+    price: z.number().min(1, { message: 'Price is required' }),
+    stock: z.number().min(1, { message: 'Stock is required' }),    
+    description: z.string().min(1 ,{ message: 'Description is required' })
 })
-
-export type PostProductPayload = z.infer<typeof PostProductPayloadSchema>
