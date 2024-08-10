dfca0fcb9c6b79451d7377d90e54cf090475fec0
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-08-10 15:21:19 +0700

    feat: change event name, add delete cart & its item

--- src/services/postgres/CartsService.ts
+++ src/services/postgres/CartsService.ts
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 import EventService from "../event/EventService";
 import { InvariantError, NotFoundError } from "@/exceptions";
-import { AddItemToCart, PostCartItem } from "@/utils/types";
+import { AddItemToCart, DeleteCartItem, PostCartItem } from "@/utils/types";
 import { nanoid } from "nanoid";
 
 export class CartsService {
@@ -18,7 +18,7 @@ export class CartsService {
         const id = `cart-${nanoid(16)}`
         
         return this.getCartByUserId(payload.userId).then(cartData => {
-            this.event.emit<AddItemToCart>('cartItemAdded', { 
+            this.event.emit<AddItemToCart>('new-cart-item', { 
                 ...payload, cartId: cartData.id 
             })
 
@@ -35,7 +35,7 @@ export class CartsService {
                 throw new InvariantError('Failed to add user cart')
             }
 
-            this.event.emit<AddItemToCart>('cartItemAdded', { 
+            this.event.emit<AddItemToCart>('new-cart-item', { 
                 ...payload, cartId: rows[0].id 
             })
 
@@ -56,5 +56,63 @@ export class CartsService {
         }
 
         return rows[0]
+    }
+
+    async getCarts(userId: string) {
+        const cart = await this.getCartByUserId(userId)
+
+        const query = {
+            text: 'SELECT * FROM cart_items WHERE cart_id = $1',
+            values: [cart.id]
+        }
+
+        const { rows } = await this.pool.query(query)
+
+        return {
+            cart: cart,
+            cart_items: rows
+        }
+    }
+
+    async deleteCarts(userId: string) {
+        const cart = await this.getCartByUserId(userId)
+    
+   
+        const deleteItemsQuery = {
+            text: 'DELETE FROM cart_items WHERE cart_id = $1',
+            values: [cart.id]
+        }
+
+        const { rowCount: itemsDeleted } = await this.pool.query(deleteItemsQuery)
+
+        if (!itemsDeleted) {
+            throw new InvariantError('Failed to delete cart items')
+        }
+
+        const deleteCartQuery = {
+            text: 'DELETE FROM carts WHERE id = $1',
+            values: [cart.id]
+        }
+
+        const { rowCount: cartDeleted } = await this.pool.query(deleteCartQuery)
+
+        if (!cartDeleted) {
+            throw new InvariantError('Failed to delete cart')
+        }
+    }
+
+    async deleteCartItem(payload: DeleteCartItem) {
+        const cart = await this.getCartByUserId(payload.userId)
+
+        const query = {
+            text: 'DELETE FROM cart_items WHERE id = $1 AND cart_id = $2',
+            values: [payload.itemId, cart.id]
+        }
+
+        const { rowCount } = await this.pool.query(query)
+
+        if (!rowCount) {
+            throw new NotFoundError('Cart item not found')
+        }
     }
 }
